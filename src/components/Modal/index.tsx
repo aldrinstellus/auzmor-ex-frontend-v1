c4d9a23c642d349d6e1168bb175c5fdf06e4449e
Rajesh Soni <rajesh.soni@american-technology.net>
2023-05-02 13:52:10 +0530

    Modal refactor

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,33 +1,19 @@
 import React, { ReactNode, Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-
-import IconButton, { Variant as IconVariant } from 'components/IconButton';
-import Icon from 'components/Icon';
-import { twConfig } from 'utils/misc';
 import clsx from 'clsx';
 
 export type ModalProps = {
   open: boolean;
-  closeModal: () => void | null;
-  body: ReactNode | null;
-  title?: string;
-  footer?: ReactNode | null;
-  onBackIconClick?: () => void;
-  showBackIcon?: boolean;
+  closeModal?: () => void | null;
+  children: ReactNode;
   className?: string;
-  wMax?: string;
 };
 
 const Modal: React.FC<ModalProps> = ({
   open,
   closeModal,
-  title = '',
-  body,
-  footer = null,
-  showBackIcon = false,
-  onBackIconClick,
-  className = '',
-  wMax = 'max-w-xl',
+  children,
+  className = 'max-w-xl',
 }) => {
   const panelStyle = clsx(
     {
@@ -35,14 +21,18 @@ const Modal: React.FC<ModalProps> = ({
         true,
     },
     {
-      [wMax]: true,
+      [className]: true,
     },
   );
 
   return (
     <>
       <Transition appear show={open} as={Fragment}>
-        <Dialog as="div" className="relative z-10 " onClose={closeModal}>
+        <Dialog
+          as="div"
+          className="relative z-10 "
+          onClose={() => (!!closeModal ? closeModal() : null)}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -66,39 +56,7 @@ const Modal: React.FC<ModalProps> = ({
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className={panelStyle}>
-                  {!!title && (
-                    <div className="flex flex-wrap border-b-1 border-neutral-200 items-center">
-                      {showBackIcon && (
-                        <Icon
-                          name="arrowLeftOutline"
-                          stroke={twConfig.theme.colors.neutral['900']}
-                          className="ml-4"
-                          size={16}
-                          onClick={onBackIconClick}
-                        />
-                      )}
-
-                      <Dialog.Title
-                        as="h3"
-                        className="text-lg text-black p-4 font-extrabold flex-[50%]"
-                      >
-                        {title}
-                      </Dialog.Title>
-                      <IconButton
-                        onClick={closeModal}
-                        icon={'close'}
-                        className="!flex-[0] !text-right !p-1 !mx-4 !my-3 !bg-inherit !text-neutral-900"
-                        variant={IconVariant.Primary}
-                      />
-                    </div>
-                  )}
-                  <div>
-                    <div className="text-sm text-neutral-900">{body}</div>
-                  </div>
-
-                  {!!footer && <div className="bg-blue-50">{footer}</div>}
-                </Dialog.Panel>
+                <Dialog.Panel className={panelStyle}>{children}</Dialog.Panel>
               </Transition.Child>
             </div>
           </div>
