850d7992fc5e2c89ca53bcc91e5f5fde58007d70
ANISH SARKAR <anish.sarkar@auzmor.com>
2023-05-05 13:34:32 +0530

    remove unused codes

--- src/components/ActivityFeed/index.tsx
+++ src/components/ActivityFeed/index.tsx
@@ -8,10 +8,7 @@ import { IPost } from 'queries/post';
 
 import { InfiniteScroll } from 'components/InfiniteScroll';
 import CreatePostCard from 'components/PostBuilder/components/CreatePostCard';
-import Button, { Size, Variant } from 'components/Button';
 import Icon from 'components/Icon';
-import { useMutation } from '@tanstack/react-query';
-import { announcementRead } from 'queries/post';
 
 type ActivityFeedProps = {
   activityFeed: any;
@@ -24,15 +21,6 @@ const ActivityFeed: React.FC<ActivityFeedProps> = ({
   loadMore,
   setShowModal,
 }) => {
-  // const acknowledgeAnnouncement = useMutation({
-  //   mutationKey: ['acknowledgeAnnouncement'],
-  //   mutationFn: announcementRead,
-  //   onError: (error) => console.log(error),
-  //   onSuccess: (data, variables, context) => {
-  //     console.log('data==>', data);
-  //   },
-  // });
-
   return (
     <div>
       {/* <div className="flex flex-row items-center gap-x-2">
@@ -50,31 +38,6 @@ const ActivityFeed: React.FC<ActivityFeedProps> = ({
             itemCount={activityFeed.length}
             itemRenderer={(index) => (
               <div key={`post-${index}`} className="mt-8">
-                {/* {activityFeed[index].isAnnouncement && (
-                  <div className="flex justify-between items-center bg-blue-700 -mb-4 p-2 rounded-t-9xl">
-                    <div className="flex justify-center items-center text-white text-xs font-bold space-x-4">
-                      <div>
-                        <Icon name="flashIcon" />
-                      </div>
-                      <div className="text-xs font-bold">Announcement</div>
-                    </div>
-                    <Button
-                      className="text-sm font-bold"
-                      label={'Mark as read'}
-                      size={Size.Small}
-                      variant={Variant.Tertiary}
-                      onClick={() => {
-                        acknowledgeAnnouncement.mutate({
-                          entityId: activityFeed?.[index]?.id,
-                          entityType: 'post',
-                          type: activityFeed?.[index]?.myReactions?.[0].type,
-                          reaction:
-                            activityFeed?.[index]?.myReactions?.[0].reaction,
-                        });
-                      }}
-                    />
-                  </div>
-                )} */}
                 <Post data={activityFeed[index]} />
               </div>
             )}
