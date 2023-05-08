f26e016a97e5429f689f5d7b167621b22da36ae1
Akshay Rajpurohit <akshay.r@auzmor.com>
2023-05-08 09:20:49 +0530

    Refactor code

--- src/components/RenderPost/components/Mention.tsx
+++ src/components/RenderPost/components/Mention.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement } from 'react';
 import MentionUserCard from './MentionUserCard';
 import useHover from 'hooks/useHover';
 
