2a552004e7842350676fd2cfcc3d639daa4c32ac
Akshay Rajpurohit <akshay.r@auzmor.com>
2023-05-09 13:46:20 +0530

    Center the loader gif

--- src/components/Image/components/Loader.tsx
+++ src/components/Image/components/Loader.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react';
 
 const Loader: React.FC = (): ReactElement => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center w-full h-full">
       <img src="https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif" />
     </div>
   );
