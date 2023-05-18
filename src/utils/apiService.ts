43313418ef73cab78260db20ed1525d9b38b0452
Rajesh Soni <rajesh.soni@american-technology.net>
2023-05-18 12:37:02 +0530

    query string formating

--- src/utils/apiService.ts
+++ src/utils/apiService.ts
@@ -1,3 +1,4 @@
+import qs from 'qs';
 import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 
 import { getItem } from './persist';
@@ -32,7 +33,12 @@ class ApiService {
   }
 
   async get(url: string, params = {}) {
-    return await this.instance.get(url, { params });
+    const _params = qs.stringify(params, { arrayFormat: 'repeat' });
+    let _url = url;
+    if (_params) {
+      _url += `?${_params}`;
+    }
+    return await this.instance.get(_url);
   }
 
   async put(url: string, data = {}) {
