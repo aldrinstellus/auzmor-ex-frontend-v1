51f49a7d8fdcefb32082639c633037ce192f3115
Rajesh Soni <rajesh.soni@american-technology.net>
2023-05-25 16:02:05 +0530

    SSO config and logout screen

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,14 +4,23 @@ import clsx from 'clsx';
 export type CardProps = {
   children?: ReactNode;
   className?: string;
+  dataTestId?: string;
 };
 
-const Card: React.FC<CardProps> = ({ children, className = '' }) => {
+const Card: React.FC<CardProps> = ({
+  children,
+  className = '',
+  dataTestId = '',
+}) => {
   const cardStyle = clsx(
     { 'bg-white shadow rounded-9xl': true },
     { [className]: true },
   );
-  return <div className={cardStyle}>{children}</div>;
+  return (
+    <div className={cardStyle} data-testid={dataTestId}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
