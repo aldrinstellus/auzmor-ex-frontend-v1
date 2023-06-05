d4901e9ce3a267007b3f55065977976263ff4161
Akshay Rajpurohit <akshay.r@auzmor.com>
2023-06-06 01:33:45 +0530

    AO-432 | Add animation to modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -31,47 +31,18 @@ const Modal: React.FC<ModalProps> = ({
   );
 
   return (
-    // <>
-    //   <Transition appear show={open} as={Fragment}>
-    //     <Dialog
-    //       as="div"
-    //       className="relative z-10 "
-    //       onClose={() => (!!closeModal ? closeModal() : null)}
-    //     >
-    //       <Transition.Child
-    //         as={Fragment}
-    //         enter="ease-out duration-300"
-    //         enterFrom="opacity-0"
-    //         enterTo="opacity-100"
-    //         leave="ease-in duration-200"
-    //         leaveFrom="opacity-100"
-    //         leaveTo="opacity-0"
-    //       >
-    //         <div className="fixed inset-0 bg-black bg-opacity-25" />
-    //       </Transition.Child>
-
-    //       <div className="fixed inset-0 overflow-y-auto">
-    //         <div className="flex min-h-full items-center justify-center p-4 text-center">
-    //           <Transition.Child
-    //             as={Fragment}
-    //             enter="ease-out duration-300"
-    //             enterFrom="opacity-0 scale-95"
-    //             enterTo="opacity-100 scale-100"
-    //             leave="ease-in duration-200"
-    //             leaveFrom="opacity-100 scale-100"
-    //             leaveTo="opacity-0 scale-95"
-    //           >
-    //             <Dialog.Panel className={panelStyle}>{children}</Dialog.Panel>
-    //           </Transition.Child>
-    //         </div>
-    //       </div>
-    //     </Dialog>
-    //   </Transition>
-    // </>
-    open ? (
+    <div
+      className={`fixed inset-0 flex items-center z-50
+       transition-colors ${open ? 'visible bg-black/60' : 'invisible'}
+       backdrop-blur-sm`}
+      onClick={closeModal}
+    >
       <div
-        className="fixed inset-0 top-0 left-0 right-0 w-screen h-screen bg-black/60 z-50 flex justify-center items-center"
-        onClick={closeModal}
+        className={`
+        flex justify-center
+        min-w-full
+        transition-all duration-200
+      ${open ? 'scale-100 opacity-100' : 'scale-110 opacity-0'}`}
       >
         {showModalCloseBtn && (
           <div
@@ -89,9 +60,7 @@ const Modal: React.FC<ModalProps> = ({
           {children}
         </div>
       </div>
-    ) : (
-      <></>
-    )
+    </div>
   );
 };
 
