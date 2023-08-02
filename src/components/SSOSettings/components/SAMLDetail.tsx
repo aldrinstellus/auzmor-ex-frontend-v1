4e090af7a25afdb53006a0e40b77ff7be7409eab
darshakparmar01 <darshak.p@auzmor.com>
2023-08-02 13:46:57 +0530

    AO-846 Admin Setting Screen

--- src/components/SSOSettings/components/SAMLDetail.tsx
+++ src/components/SSOSettings/components/SAMLDetail.tsx
@@ -16,40 +16,43 @@ const SAMLDetail: React.FC<SAMLDetailProps> = ({
   value,
 }): ReactElement => {
   return (
-    <div className="flex items-center justify-between p-6">
-      <p className="font-normal text-neutral-500">{prop}</p>
-      <div className="flex items-center">
-        <p className="font-bold text-neutral-900">{value}</p>
-        <Icon
-          className="ml-2 mb-1"
-          name="copy"
-          disabled
-          onClick={() => {
-            navigator.clipboard.writeText(value);
-            toast(
-              <SuccessToast
-                content={`${prop} has been copied to your clipboard!`}
-              />,
-              {
-                closeButton: (
-                  <Icon
-                    name="closeCircleOutline"
-                    stroke={twConfig.theme.colors.primary['500']}
-                    size={20}
-                  />
-                ),
-                style: {
-                  border: `1px solid ${twConfig.theme.colors.primary['300']}`,
-                  borderRadius: '6px',
-                  display: 'fixed',
-                  alignItems: 'center',
+    <div className="flex items-center space-x-4">
+      <p className="font-normal text-xs text-neutral-500 w-[121px]">{prop}</p>
+      <div className="flex items-center space-x-2">
+        <p className="font-bold text-sm text-neutral-900">{value}</p>
+        {value && (
+          <Icon
+            className=""
+            name="copy"
+            size={16}
+            disabled
+            onClick={() => {
+              navigator.clipboard.writeText(value);
+              toast(
+                <SuccessToast
+                  content={`${prop} has been copied to your clipboard!`}
+                />,
+                {
+                  closeButton: (
+                    <Icon
+                      name="closeCircleOutline"
+                      stroke={twConfig.theme.colors.primary['500']}
+                      size={20}
+                    />
+                  ),
+                  style: {
+                    border: `1px solid ${twConfig.theme.colors.primary['300']}`,
+                    borderRadius: '6px',
+                    display: 'fixed',
+                    alignItems: 'center',
+                  },
+                  autoClose: TOAST_AUTOCLOSE_TIME,
+                  transition: slideInAndOutTop,
                 },
-                autoClose: TOAST_AUTOCLOSE_TIME,
-                transition: slideInAndOutTop,
-              },
-            );
-          }}
-        />
+              );
+            }}
+          />
+        )}
       </div>
     </div>
   );
