39ae0070e3e3d19b9a4a21ccc1272a9483c117b5
DhruvinModi <dhruvin.m@american-technology.net>
2023-08-11 09:12:27 +0530

    Time validation

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,11 +1,20 @@
 import moment from 'moment';
 import timezones from './timezones.json';
 
+export const TIME_PATTERN = /^(0[0-9]|1[0-2]):[0-5][0-9] (am|pm)$/; // it will strictly follow "HH:MM am/pm, HH-> [00-12], MM-> [00->59]"
+
 export const afterXUnit = (
   x: number,
   unit: moment.unitOfTime.DurationConstructor,
 ) => {
   return moment().add(x, unit);
+};
+
+export const beforeXUnit = (
+  x: number,
+  unit: moment.unitOfTime.DurationConstructor,
+) => {
+  return moment().subtract(x, unit);
 };
 
 export const humanizeTime = (time: string) => {
