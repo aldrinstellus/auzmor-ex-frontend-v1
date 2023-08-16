045adf4914fa4a0039688069ace6698a2943a8de
darshakparmar01 <darshak.p@auzmor.com>
2023-08-16 20:13:38 +0530

    AO-130 Team Detail Add members functionality

--- src/pages/Users/components/TeamDetail/index.tsx
+++ src/pages/Users/components/TeamDetail/index.tsx
@@ -1,6 +1,6 @@
 import Button from 'components/Button';
 import Icon from 'components/Icon';
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'components/Card';
 import { ITeamDetailState } from 'pages/Users';
 import People from '../People';
@@ -10,17 +10,31 @@ import EntitySearchModal, {
 import Tooltip from 'components/Tooltip';
 import PopupMenu from 'components/PopupMenu';
 import { TeamFlow } from '../Teams';
+import { IGetUser } from 'queries/users';
+import Avatar from 'components/Avatar';
+import { addTeamMember } from 'queries/teams';
+import FailureToast from 'components/Toast/variants/FailureToast';
+import { toast } from 'react-toastify';
+import { useMutation } from '@tanstack/react-query';
+import { twConfig } from 'utils/misc';
+import { TOAST_AUTOCLOSE_TIME } from 'utils/constants';
+import { slideInAndOutTop } from 'utils/react-toastify';
+import queryClient from 'utils/queryClient';
+import SuccessToast from 'components/Toast/variants/SuccessToast';
+import TeamMember from './TeamMembers';
 
 export interface ITeamMemberProps {
   id?: string;
   name?: string;
-  category?: string;
+  category?: Record<string, string>;
   description?: string;
   teamMembers?: number;
   teamTab: string;
   openModal: () => void;
   setShowTeamDetail: (detail: ITeamDetailState) => void;
-  setTeamFlow: any;
+  showAddMemberModal: boolean;
+  openAddMemberModal: () => void;
+  closeAddMemberModal: () => void;
 }
 
 const TeamDetail: React.FC<ITeamMemberProps> = ({
@@ -30,10 +44,60 @@ const TeamDetail: React.FC<ITeamMemberProps> = ({
   category,
   teamMembers,
   teamTab,
-  // openModal,
+  openModal,
   setShowTeamDetail,
-  // setTeamFlow,
+  showAddMemberModal,
+  openAddMemberModal,
+  closeAddMemberModal,
 }) => {
+  const addTeamMemberMutation = useMutation({
+    mutationKey: ['add-team-member', id],
+    mutationFn: (payload: any) => {
+      return addTeamMember(id || '', payload);
+    },
+    onError: (error: any) => {
+      toast(
+        <FailureToast
+          content={`Error Adding Team Members`}
+          dataTestId="team-create-error-toaster"
+        />,
+        {
+          closeButton: (
+            <Icon
+              name="closeCircleOutline"
+              stroke={twConfig.theme.colors.red['500']}
+              size={20}
+            />
+          ),
+          style: {
+            border: `1px solid ${twConfig.theme.colors.red['300']}`,
+            borderRadius: '6px',
+            display: 'flex',
+            alignItems: 'center',
+          },
+          autoClose: TOAST_AUTOCLOSE_TIME,
+          transition: slideInAndOutTop,
+          theme: 'dark',
+        },
+      );
+    },
+    onSuccess: (data: any) => {
+      toast(<SuccessToast content={'Members has been added to team'} />, {
+        style: {
+          border: `1px solid ${twConfig.theme.colors.primary['300']}`,
+          borderRadius: '6px',
+          display: 'flex',
+          alignItems: 'center',
+        },
+        autoClose: TOAST_AUTOCLOSE_TIME,
+        transition: slideInAndOutTop,
+        theme: 'dark',
+      });
+      queryClient.invalidateQueries(['categories']);
+      queryClient.invalidateQueries(['team-members']);
+    },
+  });
+
   return (
     <>
       <Card className="py-8 space-y-6">
@@ -70,9 +134,7 @@ const TeamDetail: React.FC<ITeamMemberProps> = ({
               className="flex space-x-1"
               label="Add Members"
               leftIcon="add"
-              onClick={() => {
-                // add memeber modal
-              }}
+              onClick={openAddMemberModal}
               dataTestId="add-members-btn"
             />
           </Tooltip>
@@ -91,7 +153,7 @@ const TeamDetail: React.FC<ITeamMemberProps> = ({
                 Team type
               </div>
               <div className="text-xl font-semibold">
-                {category || 'category'}
+                {category?.name || 'category'}
               </div>
             </div>
             <div className="flex flex-col space-y-2">
@@ -101,7 +163,7 @@ const TeamDetail: React.FC<ITeamMemberProps> = ({
               <div className="text-xl font-semibold">{teamMembers || 0}</div>
             </div>
             <div>
-              {/* <PopupMenu
+              <PopupMenu
                 triggerNode={
                   <div className="cursor-pointer">
                     <Icon name="setting" stroke="#171717" />
@@ -111,11 +173,9 @@ const TeamDetail: React.FC<ITeamMemberProps> = ({
                   {
                     icon: 'edit',
                     label: 'Edit',
-                    onClick: () => {
-                      openModal();
-                      setTeamFlow(TeamFlow.EditTeam);
-                      id && setTeamId(id);
-                    },
+                    // onClick: () => {
+                    //   setTeamFlow(TeamFlow.EditTeam);
+                    // },
                     dataTestId: 'team-edit',
                     permissions: [''],
                   },
@@ -128,32 +188,82 @@ const TeamDetail: React.FC<ITeamMemberProps> = ({
                   {
                     icon: 'cancel',
                     label: 'Remove',
-                    onClick: () => openDeleteModal(),
+                    // onClick: () => openDeleteModal(),
                     dataTestId: 'team-remove',
                     permissions: [''],
                   },
                 ]}
                 className=" right-20 w-44"
-              /> */}
+              />
             </div>
           </div>
         </div>
         <div className="px-8">
-          <People
+          {/* <TeamMember
             showModal={false}
             openModal={() => {}}
             closeModal={() => {}}
             teamTab={teamTab}
             teamId={id}
-          />
+          /> */}
         </div>
       </Card>
-      <EntitySearchModal
-        entityType={EntitySearchModalType.Team}
-        onSubmit={() => {}}
-        title="Add Members"
-        submitButtonText="add Members"
-      />
+      {showAddMemberModal && (
+        <EntitySearchModal
+          open={showAddMemberModal}
+          openModal={openAddMemberModal}
+          closeModal={closeAddMemberModal}
+          entityType={EntitySearchModalType.User}
+          entityRenderer={(data: IGetUser) => {
+            return (
+              <div className="flex space-x-4 w-full">
+                <Avatar
+                  name={data?.fullName || 'U'}
+                  size={32}
+                  image={data?.profileImage?.original}
+                />
+                <div className="flex space-x-6 w-full">
+                  <div className="flex flex-col w-full">
+                    <div className="flex justify-between items-center">
+                      <div className="text-sm font-bold text-neutral-900">
+                        {data?.fullName}
+                      </div>
+                      {/* <div className="flex space-x-[14px] items-center">
+                        <div className="flex space-x-1 items-start">
+                          <Icon name="briefcase" size={16} />
+                          <div className="text-xs font-normal text-neutral-500">
+                            {'Chief Financial officer'}
+                          </div>
+                        </div>
+
+                        <div className="w-1 h-1 bg-neutral-500 rounded-full" />
+
+                        <div className="flex space-x-1 items-start">
+                          <Icon name="location" size={16} />
+                          <div className="text-xs font-normal text-neutral-500">
+                            {'New York, US.'}
+                          </div>
+                        </div>
+                      </div> */}
+                    </div>
+                    <div className="text-xs font-normal text-neutral-500">
+                      {data?.primaryEmail}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          }}
+          onSubmit={(userIds: string[]) => {
+            addTeamMemberMutation.mutate({ userIds: userIds });
+            closeAddMemberModal();
+          }}
+          title="Add Members"
+          submitButtonText="Add Members"
+          onCancel={closeAddMemberModal}
+          cancelButtonText="Cancel"
+        />
+      )}
     </>
   );
 };
