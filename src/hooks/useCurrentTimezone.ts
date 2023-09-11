a097e9bb7f800802f6e64bd201f2011c48081ce4
Seram Nitesh Singh <nitesh.s@auzmor.com>
2023-09-11 10:28:19 +0530

    Add default timezeone as browser timezone

--- src/hooks/useCurrentTimezone.ts
+++ src/hooks/useCurrentTimezone.ts
@@ -2,5 +2,12 @@ import { useCurrentUser } from 'queries/users';
 
 export const useCurrentTimezone = () => {
   const { data, isLoading } = useCurrentUser();
-  return { isLoading, currentTimezone: data?.data?.result?.data?.timeZone };
+  const browserTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  return {
+    isLoading,
+    currentTimezone:
+      data?.data?.result?.data?.timeZone || browserTimezone === 'Asia/Calcutta'
+        ? 'Asia/Kolkata'
+        : browserTimezone,
+  };
 };
