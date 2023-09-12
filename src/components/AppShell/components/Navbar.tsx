a6879eff8c58af9d143fcd48e25b123c31edf8c0
Shubhdeep Chhabra <shubhdeep.c@auzmor.com>
2023-09-12 16:16:14 +0530

    navbar fixes

--- src/components/AppShell/components/Navbar.tsx
+++ src/components/AppShell/components/Navbar.tsx
@@ -78,8 +78,8 @@ const Navbar = () => {
 
   return (
     <header className="sticky top-0 z-50 bg-white shadow h-16 w-full py-[2px]">
-      <div className="bg-white h-full w-full max-w-[1440px] flex items-center justify-center py-0.5 px-8 gap-[60px] mx-auto">
-        <Link to="/feed" data-testid="auzmor-office" className="flex-1">
+      <div className="bg-white h-full w-full max-w-[1440px] flex items-center py-0.5 px-8 mx-auto justify-between">
+        <Link to="/feed" data-testid="auzmor-office">
           <Logo />
         </Link>
         {/* <div className="flex-1">
@@ -97,24 +97,24 @@ const Navbar = () => {
             ]}
           />
         </div> */}
-        <div className="flex items-center gap-10">
-          {navigations.map((nav) => (
-            <NavbarMenuItem nav={nav} key={nav.label} />
-          ))}
-        </div>
-        <div className="h-full">
-          <Divider variant={Variant.Vertical} />
-        </div>
-        <div className="flex items-center gap-10">
-          {isAdmin &&
-            adminNavigations.map((nav) => (
+        <div className="flex items-center gap-[60px] h-full">
+          <div className="flex items-center gap-10">
+            {navigations.map((nav) => (
               <NavbarMenuItem nav={nav} key={nav.label} />
             ))}
-          <div>
-            <NotificationsOverview />
           </div>
-          <div className="p-2">
-            <AccountCard />
+          <Divider className="h-full" variant={Variant.Vertical} />
+          <div className="flex items-center gap-10">
+            {isAdmin &&
+              adminNavigations.map((nav) => (
+                <NavbarMenuItem nav={nav} key={nav.label} />
+              ))}
+            <div>
+              <NotificationsOverview />
+            </div>
+            <div className="p-2">
+              <AccountCard />
+            </div>
           </div>
         </div>
       </div>
