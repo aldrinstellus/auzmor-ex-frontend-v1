79393c960b384408580b8ab5d44ad18d265221bf
DhruvinModi <dhruvin.m@american-technology.net>
2023-12-07 11:53:32 +0530

    Bug fix 1. Remove unused global constant variables 2. Update valid hexcode length to 6 chars only

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,25 +11,7 @@ export const URL_REGEX =
 // Found this regex here - https://stackoverflow.com/questions/16369642/javascript-how-to-use-a-regular-expression-to-remove-blank-lines-from-a-string
 export const EMPTY_REGEX = /^\s*$(?:\r\n?|\n){3,}/gm;
 
-export const BRANDING = {
-  primaryColor: '#119dbd',
-  secondaryColor: '#F97316',
-  pageTitle: 'Page Title',
-  favicon: 'https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg',
-  logo: 'https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg',
-  loginConfig: {
-    layout: 'RIGHT', // LEFT | RIGHT | CENTER Default: RIGHT
-    backgroundType: 'IMAGE', // IMAGE | VIDEO | COLOR  Default: IMAGE
-    image:
-      'https://images.hdqwalls.com/download/reindeer-silhouette-grazing-in-the-field-4n-3840x2160.jpg',
-    video:
-      'https://media.istockphoto.com/id/1222958227/video/big-sur-bixby-bridge-route-1-california-state-route-atlantic-ocean-aerial-drone.mp4?s=mp4-640x640-is&k=20&c=UslDy-nOXnXWubMOzXV3Ya3p7xMJbuA2W71X-oyNArM=',
-    text: 'Welcome to auzmor office',
-    color: '#123456',
-  },
-};
-
 export const MB = 1 * 1024 * 1024; // 1 MB
 export const KB = 1 * 1024; // 1 KB
 
-export const HEX_REGEX = /^#([0-9a-f]{3}){1,2}$/i;
+export const HEX_REGEX = /^#([0-9a-f]{6})$/i;
