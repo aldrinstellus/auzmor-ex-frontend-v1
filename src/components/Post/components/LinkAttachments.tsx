77a7bde60a7a478e7ec16aedc1d41b6b4539a342
Anshul Verma <anshul.verma@american-technology.net>
2024-04-05 14:02:37 +0530

    code refactor

--- src/components/Post/components/LinkAttachments.tsx
+++ src/components/Post/components/LinkAttachments.tsx
@@ -1,7 +1,7 @@
 import Icon from 'components/Icon';
 import moment from 'moment';
 import { LinkAttachment } from 'queries/post';
-import React, { FC } from 'react';
+import { FC } from 'react';
 import { ProductEnum } from 'utils/apiService';
 import { getItem } from 'utils/persist';
 
@@ -10,36 +10,39 @@ interface ILinkAttachmentsProps {
 }
 
 const LinkAttachments: FC<ILinkAttachmentsProps> = ({ attachments }) => {
-  const firstAttachment = attachments[0];
-
-  if (!firstAttachment) {
-    return null;
-  }
-
-  const { url, title } = firstAttachment;
-
-  const isImage = /(?:\.jpg|\.png)$/i.test(title);
-  const previewUrl = `${getItem(`${ProductEnum.Learn}RegionUrl`)}/attachments/${
-    url.split('/attachments/')[1].split('/')[0]
-  }/preview?auth_token=${getItem('uat')}&t=${moment()}`;
+  const getAuthLearnUrl = `${getItem(
+    `${ProductEnum.Learn}RegionUrl`,
+  )}/attachments/`;
+  const isImageRegex = /(?:\.jpg|\.png)$/i;
 
   return (
     <div className="flex gap-2">
-      <div
-        key={firstAttachment._id}
-        onClick={() => window.open(previewUrl)}
-        className="flex  p-2 rounded-9xl border border-neutral-200 w-[173px] justify-center items-center gap-2 cursor-pointer hover:shadow-lg transition"
-      >
-        {isImage && (
-          <div className="flex w-6 h-6">
-            <img src={previewUrl} />
+      {attachments.map((each) => {
+        const attachmentUrl = each?.url.split('/attachments/')[1].split('/')[0];
+        const previewUrl = `${getAuthLearnUrl}${attachmentUrl}/preview?auth_token=${getItem(
+          'uat',
+        )}&t=${moment()}`;
+
+        return (
+          <div
+            key={each._id}
+            onClick={() => window.open(previewUrl)}
+            className="flex p-2 rounded-9xl border border-neutral-200 w-[173px] justify-center items-center gap-2 cursor-pointer hover:shadow-lg transition"
+          >
+            {isImageRegex.test(each?.title) && (
+              <div className="flex w-6 h-6">
+                <img src={previewUrl} />
+              </div>
+            )}
+            <Icon
+              name={each?.title?.substring(each?.title.lastIndexOf('.') + 1)}
+            />
+            <p className="text-xs font-medium max-w-[124px] truncate">
+              {each?.title.substring(0, each?.title.lastIndexOf('.'))}
+            </p>
           </div>
-        )}
-        <Icon name={title.substring(title.lastIndexOf('.') + 1)} />
-        <p className="text-xs font-medium max-w-[124px] truncate">
-          {title.substring(0, title.lastIndexOf('.'))}
-        </p>
-      </div>
+        );
+      })}
     </div>
   );
 };
