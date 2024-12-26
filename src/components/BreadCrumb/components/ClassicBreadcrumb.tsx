a2f690b4248805984d5a03e58cb6e5f7011b9c80
DhruvinModi <dhruvin.m@american-technology.net>
2024-12-26 19:19:54 +0530

    Optimize location column rendering

--- src/components/BreadCrumb/components/ClassicBreadcrumb.tsx
+++ src/components/BreadCrumb/components/ClassicBreadcrumb.tsx
@@ -1,7 +1,6 @@
 import Icon from 'components/Icon';
-import React, { FC, useEffect, useRef, useState } from 'react';
+import React, { FC } from 'react';
 import { Item } from 'contexts/DocumentPathContext';
-import TruncateMarkup from 'react-truncate-markup';
 
 interface IClassicBreadcrumbProps {
   items: Item[];
@@ -14,42 +13,22 @@ const ClassicBreadcrumb: FC<IClassicBreadcrumbProps> = ({
   className = '',
   onItemClick = () => {},
 }) => {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [sliceIndex, setSliceIndex] = useState<number>(items.length);
-
-  useEffect(() => {
-    if (ref.current) {
-      if (ref.current.clientWidth < ref.current.scrollWidth) {
-        if (sliceIndex > 1) {
-          setSliceIndex(sliceIndex - 1);
-        } else {
-          setSliceIndex(1);
-        }
-      }
-    }
-  }, [items, sliceIndex]);
-
   return (
-    <div className="flex items-center gap-2 w-full" ref={ref}>
-      {items.slice(0, sliceIndex).map((each, index) => (
+    <div className="flex items-center gap-2 w-full overflow-hidden">
+      {items.map((each, index) => (
         <div key={each.id} className="flex items-center gap-2 h-6">
           <Icon name="folder" size={20} />
-          <TruncateMarkup>
-            <span
-              className={`flex font-medium text-neutral-500 cursor-pointer ${
-                index === items.length - 1 &&
-                'font-bold text-neutral-900 cursor-default'
-              } ${className}`}
-              onClick={() => onItemClick(each)}
-            >
-              {each.label}
-            </span>
-          </TruncateMarkup>
+          <span
+            className={`flex font-medium text-neutral-500 cursor-pointer truncate ${
+              index === items.length - 1 &&
+              'font-bold text-neutral-900 cursor-default'
+            } ${className}`}
+            onClick={() => onItemClick(each)}
+          >
+            {each.label}
+          </span>
           {index < items.length - 1 && (
             <Icon name="arrowRight" size={16} hover={false} />
-          )}
-          {index === sliceIndex - 1 && index !== items.length - 1 && (
-            <span>...</span>
           )}
         </div>
       ))}
