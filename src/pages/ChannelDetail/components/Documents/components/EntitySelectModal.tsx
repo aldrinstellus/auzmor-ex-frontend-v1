5a7d38efa56a00220a3a45397670f8b2fa552ae7
DhruvinModi <dhruvin.m@american-technology.net>
2025-02-03 12:29:58 +0530

    Enable double click for folder navigation on select folder modal

--- src/pages/ChannelDetail/components/Documents/components/EntitySelectModal.tsx
+++ src/pages/ChannelDetail/components/Documents/components/EntitySelectModal.tsx
@@ -213,6 +213,7 @@ const EntitySelectModal: FC<IEntitySelectModalProps> = ({
       },
       height: 312,
       noDataFound: <NoDataFound hideClearBtn />,
+      isDoubleClickAllowed: headings === 'folder',
     },
   });
 
